928fcc851226e56047454c941cf0e8ba367a4a74
Urtkpeters <urtkpeters@gmail.com>
2017-07-08 18:55:58 -0400

    GHI409 & 434 - Add Banner for ARC & Create Slider
    Created the banner slider functionality and added the ARC banner to the
    slider.

--- lib/siteLib/page/pages/home.php
+++ lib/siteLib/page/pages/home.php
@@ -1,3 +1,108 @@
 <div id="homeBanner" class="homeBanner">
-    <a href="page/dojp"><img src="media/site/dojpBanner.jpg" /></a>
+    <div id="sliderBackButton" class="sliderButton sliderBackButton">&#10094;</div>
+    <div id="sliderForwardButton" class="sliderButton sliderForwardButton">&#10095;</div>
+    <div id="bannerSlide1" class="bannerSlide" style="opacity: 1;">
+        <a href="page/dojp"><img id="bannerImage" src="media/site/dojpBanner.jpg" /></a>
+    </div>
+    <div id="bannerSlide2" class="bannerSlide" style="opacity: 0;">
+        <a href="page/arc"><img id="bannerImage" src="media/site/ARCBanner.jpg" /></a>
+    </div>
 </div>
+<script>
+    var slideIndex = 1;
+    var numberOfSlides = 2;
+    var pauseSlider = false;
+    var resetSlider = false;
+
+    for(var i = 1; i <= numberOfSlides; i++)
+    {
+        if(i == 1)
+        {
+            $('#bannerSlide' + i).css('z-index', 2);
+        }
+        else
+        {
+            $('#bannerSlide' + i).css('opacity', 0);
+        }
+    }
+
+    $('#sliderForwardButton').click(function()
+    {
+        changeSlide('forward');
+    });
+
+    $('#sliderBackButton').click(function()
+    {
+        changeSlide('backward');
+    });
+
+    $(function()
+    {
+        $('#bannerImage').hover(stopSlider, resumeSlider);
+        $('#sliderBackButton').hover(stopSlider, resumeSlider);
+        $('#sliderForwardButton').hover(stopSlider, resumeSlider);
+    });
+
+    function queueSlide()
+    {
+        if(!pauseSlider)
+        {
+            changeSlide('forward');
+            setTimeout(queueSlide, 5000);
+        }
+        else
+        {
+            resetSlider = true;
+        }
+    }
+
+    function changeSlide(direction)
+    {
+        if(slideIndex < numberOfSlides && direction == 'forward')
+        {
+            slideIndex += 1;
+        }
+        else if(slideIndex == numberOfSlides && direction == 'forward')
+        {
+            slideIndex = 1;
+        }
+        else if(slideIndex > 1 && direction == 'backward')
+        {
+            slideIndex -= 1;
+        }
+        else if(slideIndex == 1 && direction == 'backward')
+        {
+            slideIndex = numberOfSlides;
+        }
+
+        for(var i = 1; i <= numberOfSlides; i++)
+        {
+            if(i != slideIndex)
+            {
+                $('#bannerSlide' + i).animate({ opacity: 0 }, 750);
+            }
+            else
+            {
+                $('#bannerSlide' + i).animate({ opacity: 1 }, 750);
+            }
+        }
+    }
+
+    function stopSlider()
+    {
+        pauseSlider = true;
+    }
+
+    function resumeSlider()
+    {
+        pauseSlider = false;
+
+        if(resetSlider)
+        {
+            resetSlider = false;
+            setTimeout(queueSlide, 5000);
+        }
+    }
+
+    setTimeout(queueSlide, 8000);
+</script>
