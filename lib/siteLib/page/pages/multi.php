b2506f5ba9cf6551871c1b5f768a6d56c966aaa7
Urtkpeters <urtkpeters@gmail.com>
2017-10-20 01:51:15 -0400

    The Bunslingers - 0.1
    Initial commit for The Bunslingers. Just rough tests.

---
+++ lib/siteLib/page/pages/multi.php
@@ -0,0 +1,5 @@
+<div id="gameDiv" class="gameDiv">
+    <canvas id="gameCanvas" class="gameCanvas" width="800" height="600"></canvas>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.3/socket.io.js"></script>
+<script src="js/multi.js"></script>
